913c3ab415910a4c9022f66c1d61e0337c47bd1a
AntoinePJr <antoinepjr@gmail.com>
2023-01-29 15:25:49 -0500

    3rd Commit, Lab#2

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,8 +102,7 @@ class Enrollment(models.Model):
     # Has question content
     # Other fields and methods you would like to design
 class Question(models.Model):
-    # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    # Foreign key to course
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # question text
     question_text = models.CharField(max_length=1000)
